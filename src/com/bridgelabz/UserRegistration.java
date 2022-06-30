fb336c588d5f12429c0fed22881b152d779db7a9
shravkv <sravan.k.vodnala@gmail.com>
2022-06-30 14:05:55 +0530

    Added: Check mobile format

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -8,6 +8,8 @@ public class UserRegistration {
         System.out.println("Welcome to UserRegistration program using Regex ");
         boolean email = Pattern.matches("[a-z]{3,}.[a-z]{3,}[\\d]{2,}@[a-z]{3,}.[a-z]{2,}", "sravan.sunny81@gmail.com");
         System.out.println("Email :"+email);
+        boolean phoneNumber = Pattern.matches("[+]91 [6789]\\d{9}", "+91 9052382248");
+        System.out.println("PhoneNumber :"+phoneNumber);
     }
 
 }
