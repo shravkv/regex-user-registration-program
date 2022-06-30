0444c45a73aeaca00908b54e42c3cfa2d773cfed
shravkv <sravan.k.vodnala@gmail.com>
2022-06-30 14:25:51 +0530

    Added: Creating password with minimum 8 characters

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -6,10 +6,17 @@ public class UserRegistration {
 
     public static void main(String[] args) {
         System.out.println("Welcome to UserRegistration program using Regex ");
+        boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", "Vodnala");
+        System.out.println("FirstName : "+firstName);
+        boolean lastName = Pattern.matches("[A-Z][a-z]{3,}", "Sravan");
+        System.out.println("LastName : "+lastName);
         boolean email = Pattern.matches("[a-z]{3,}.[a-z]{3,}[\\d]{2,}@[a-z]{3,}.[a-z]{2,}", "sravan.sunny81@gmail.com");
         System.out.println("Email :"+email);
         boolean phoneNumber = Pattern.matches("[+]91 [6789]\\d{9}", "+91 9052382248");
         System.out.println("PhoneNumber :"+phoneNumber);
+        boolean passWord = Pattern.matches("[a-z]{8,}", "sravankum");
+        System.out.println("PassWord :"+passWord);
+
     }
 
 }
